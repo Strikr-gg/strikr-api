d637fd4c6cd7d0bed6a92bf9a5c14f699d69fd9d
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-02 07:07:42 -0300

    chore: move tokens to tokens file

--- src/odyssey/prometheus/httpservice.ts
+++ src/odyssey/prometheus/httpservice.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import tokens from 'src/../tokens'
 
 export default axios.create({
   baseURL: process.env.ODYSSEY_URL,
   headers: {
-    'X-Authorization': `Bearer ${process.env.ODYSSEY_TOKEN}`,
-    'X-Refresh-Token': process.env.ODYSSEY_REFRESH_TOKEN,
+    'X-Authorization': `Bearer ${tokens.ODYSSEY_TOKEN}`,
+    'X-Refresh-Token': tokens.ODYSSEY_REFRESH_TOKEN,
   },
 })
