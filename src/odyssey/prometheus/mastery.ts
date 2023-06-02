7174c4078ee4a2ce8be810a82bd9ccf8fa263549
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-02 06:59:48 -0300

    doc: comment getPlayerMastery

--- src/odyssey/prometheus/mastery.ts
+++ src/odyssey/prometheus/mastery.ts
@@ -2,6 +2,12 @@ import { ODYSSEY } from '@types'
 import httpservice from './httpservice'
 
 export class prometheusMasteryService {
+  /**
+   * Get user carreer information from prometheus like
+   * - level
+   * - xp
+   * @param id playerId
+   */
   async getPlayerMastery(id: string) {
     const { data } = await httpservice.get<ODYSSEY.PROMETHEUS.PlayerMastery>(
       `/v1/mastery/${id}/player`,
