b629d70713319f5ce0b8e57d1ba3294686aec287
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-02 10:23:06 -0300

    chore: add regex ip to whitelist

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: [
+        '^((http?:\\/\\/)?.*?(172\\.245\\.142\\.218:3000))($|\\/.*$)',
         'http://172.245.142.218:3000',
         'http://179.106.175.51:3000',
         'https://strikr.gg',
