68020f20468e32dd165ade30dcdfa6583045c402
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-02 10:25:13 -0300

    chore: disable cors

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        '^((http?:\\/\\/)?.*?(172\\.245\\.142\\.218:3000))($|\\/.*$)',
-        'http://172.245.142.218:3000',
-        'http://179.106.175.51:3000',
-        'https://strikr.gg',
-      ],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    },
+    // cors: {
+    //   origin: [
+    //     '^((http?:\\/\\/)?.*?(172\\.245\\.142\\.218:3000))($|\\/.*$)',
+    //     'http://172.245.142.218:3000',
+    //     'http://179.106.175.51:3000',
+    //     'https://strikr.gg',
+    //   ],
+    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    //   preflightContinue: false,
+    //   optionsSuccessStatus: 204,
+    // },
   })
   await app.listen(5000)
 }
