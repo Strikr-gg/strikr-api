bcca494bb009b2aace9a4dff4252dd953ebba055
Nodgear <wbm.nodge@gmail.com>
2023-09-14 05:48:21 -0300

    feat: update players after updating leaderboard

--- src/cronjobs/leaderboard.cron.ts
+++ src/cronjobs/leaderboard.cron.ts
@@ -1,10 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { Cron } from '@nestjs/schedule'
 import { prometheusService } from 'src/odyssey/prometheus/service'
-import { PrismaClient } from '@prisma/client'
+import { Gamemode, PrismaClient } from '@prisma/client'
+import { UtilsService } from 'src/utils/utils.service'
 const prisma = new PrismaClient()
 
-const ledearboardLogger = new Logger('Leaderboard')
+const leaderboardLogger = new Logger('Leaderboard')
 
 type Regions =
   | 'Global'
@@ -38,7 +39,7 @@ export class UpdateLearderboard {
 }
 
 async function populateByBoardOffset(offset = 0, count = 25, region?: Regions) {
-  ledearboardLogger.debug(
+  leaderboardLogger.debug(
     `Updating leaderboard for ${region} with > Offset:${offset} Step: ${count}`,
   )
   const leaderboardPlayers = await prometheusService.ranked.leaderboard.players(
@@ -48,7 +49,7 @@ async function populateByBoardOffset(offset = 0, count = 25, region?: Regions) {
   )
 
   for (const player of leaderboardPlayers.players) {
-    ledearboardLogger.debug(
+    leaderboardLogger.debug(
       `Updating player ${player.playerId} #${player.rank} @ ${region}`,
     )
     try {
@@ -69,8 +70,143 @@ async function populateByBoardOffset(offset = 0, count = 25, region?: Regions) {
           username: player.username,
         },
       })
+
+      const latestPlayerObject = await prisma.player.findUnique({
+        where: {
+          id: player.playerId,
+        },
+        select: {
+          updatedAt: true,
+          ratings: {
+            select: {
+              id: true,
+              createdAt: true,
+            },
+            orderBy: {
+              createdAt: 'desc',
+            },
+            take: 1,
+          },
+        },
+      })
+
+      if (!latestPlayerObject) {
+        leaderboardLogger.debug(
+          `Inserted player ${player.playerId} #${player.rank} @ ${region} - Does not have strikr ratings saved.`,
+        )
+      }
+
+      const utilsService = new UtilsService()
+      let operation: 'create' | 'update' = 'create'
+      if (
+        latestPlayerObject?.ratings?.[0] &&
+        !utilsService.areDifferentDays(
+          latestPlayerObject.ratings[0].createdAt.toISOString(),
+          new Date().toISOString(),
+        )
+      ) {
+        operation = 'update'
+      }
+
+      const characterStats = (
+        await prometheusService.stats.player(player.playerId)
+      ).characterStats
+
+      const accountStats = await prometheusService.mastery.player(
+        player.playerId,
+      )
+
+      leaderboardLogger.debug(
+        `Inserted player ${player.playerId} #${player.rank} @ ${region} - Now updating on strikr`,
+      )
+
+      await prisma.player.update({
+        where: {
+          id: player.playerId,
+        },
+        data: {
+          characterRatings: {
+            createMany: {
+              data: [
+                ...characterStats.map((cs) => {
+                  if (cs.ratingName === 'None') {
+                    return
+                  }
+                  return {
+                    character: cs.characterId,
+                    wins: cs.roleStats.Forward.wins,
+                    losses: cs.roleStats.Forward.losses,
+                    knockouts: cs.roleStats.Forward.knockouts,
+                    scores: cs.roleStats.Forward.scores,
+                    mvp: cs.roleStats.Forward.mvp,
+                    role: 'Forward',
+                    saves: cs.roleStats.Forward.saves,
+                    assists: cs.roleStats.Forward.assists,
+                    games: cs.roleStats.Forward.games,
+                    gamemode: cs.ratingName as Gamemode,
+                  }
+                }),
+                ...characterStats.map((cs) => {
+                  if (cs.ratingName === 'None') {
+                    return
+                  }
+                  return {
+                    character: cs.characterId,
+                    wins: cs.roleStats.Goalie.wins,
+                    losses: cs.roleStats.Goalie.losses,
+                    knockouts: cs.roleStats.Goalie.knockouts,
+                    scores: cs.roleStats.Goalie.scores,
+                    mvp: cs.roleStats.Goalie.mvp,
+                    role: 'Goalie',
+                    saves: cs.roleStats.Goalie.saves,
+                    assists: cs.roleStats.Goalie.assists,
+                    games: cs.roleStats.Goalie.games,
+                    gamemode: cs.ratingName as Gamemode,
+                  }
+                }),
+              ],
+            },
+          },
+          currentXp: accountStats.currentLevelXp,
+          socialUrl: player.socialUrl,
+          emoticonId: player.emoticonId,
+          logoId: player.logoId,
+          nameplateId: player.nameplateId,
+          ratings: {
+            ...(operation === 'create'
+              ? {
+                  create: {
+                    rating: player.rating,
+                    rank: player.rank,
+                    wins: player.wins,
+                    losses: player.losses,
+                    masteryLevel: player.masteryLevel,
+                  },
+                }
+              : {
+                  update: {
+                    where: {
+                      id: latestPlayerObject.ratings[0].id,
+                    },
+                    data: {
+                      games: player.games,
+                      losses: player.losses,
+                      rank: player.rank,
+                      rating: player.rating,
+                      wins: player.wins,
+                      masteryLevel: player.masteryLevel,
+                    },
+                  },
+                }),
+          },
+        },
+      })
+
+      leaderboardLogger.debug(
+        `Inserted player ${player.playerId} #${player.rank} @ ${region} - Updated on strikr!`,
+      )
     } catch (e) {
-      ledearboardLogger.error(
+      leaderboardLogger.error(
         `Error updating player ${player.playerId} #${player.rank} @ ${region}: ${e}`,
       )
     }
